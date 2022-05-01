a4880ca4ccadb1ce9b57c3b2551c5dcf29604a1e
Kawa0413 <unesco777@docomo.ne.jp>
2022-05-02 00:39:44 +0900

    0502:error judge

--- object_detection_apri.py
+++ object_detection_apri.py
@@ -63,34 +63,34 @@ if uploaded_file is not None:
     #関数でファイルパスを指定するため読み込んだ画像を特定のフォルダに保存
     img_path = f'img/{uploaded_file.name}'
     img.save(img_path)
-    objects = detect_objects(img_path)
+    # objects = detect_objects(img_path)
 
-    #描画(矩形)
-    draw = ImageDraw.Draw(img)
-    for object in objects:
-        x = object.rectangle.x
-        y = object.rectangle.y
-        w = object.rectangle.w
-        h = object.rectangle.h
-        #object_propertyに変更されている
-        # caption = object.object
-        caption = object.object_property
+    # #描画(矩形)
+    # draw = ImageDraw.Draw(img)
+    # for object in objects:
+    #     x = object.rectangle.x
+    #     y = object.rectangle.y
+    #     w = object.rectangle.w
+    #     h = object.rectangle.h
+    #     #object_propertyに変更されている
+    #     # caption = object.object
+    #     caption = object.object_property
 
-        font = ImageFont.truetype(font='./Helvetica 400.ttf', size=30)
-        text_w, text_h = draw.textsize(caption, font=font)
+    #     font = ImageFont.truetype(font='./Helvetica 400.ttf', size=30)
+    #     text_w, text_h = draw.textsize(caption, font=font)
 
-        draw.rectangle([(x, y), (x+w, y+h)], fill=None, outline='green', width=5)
-        draw.rectangle([(x, y), (x+text_w, y+text_h)], fill='green')
-        draw.text((x, y), caption, fill='white', font=font)
+    #     draw.rectangle([(x, y), (x+w, y+h)], fill=None, outline='green', width=5)
+    #     draw.rectangle([(x, y), (x+text_w, y+text_h)], fill='green')
+    #     draw.text((x, y), caption, fill='white', font=font)
 
-    st.image(img)
+    # st.image(img)
 
-    tags_name = get_tags(img_path)
-    tags_name = ', '.join(tags_name)
+    # tags_name = get_tags(img_path)
+    # tags_name = ', '.join(tags_name)
 
 
-    st.markdown('**認識されたコンテンツタグ**')
-    st.markdown(f'>{tags_name}')
+    # st.markdown('**認識されたコンテンツタグ**')
+    # st.markdown(f'>{tags_name}')
 
 
 
